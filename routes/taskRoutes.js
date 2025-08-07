facb01d3bc1e4baa254fc87cff53aee8a1d8415a
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-08-07 12:31:34 +0545

    Minor Fix

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createTask, getTasks, updateTask, deleteTask, getTask } = require('../controllers/taskcontroller');
+const { createTask, getTasks, updateTask, deleteTask, getTask } = require('../controllers/taskController');
 const protect = require('../middleware/auth');
 const router = express.Router();
 
