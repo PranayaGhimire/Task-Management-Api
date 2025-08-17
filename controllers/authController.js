2c785a82a05d301e470072be45c18d607a9e64e1
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-08-17 21:38:30 +0545

    Added HTTP Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,9 +34,18 @@ export const loginUser = async (req,res) => {
         const isMatch = await bcrypt.compare(password,user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid credentials'});
 
+        const token = generateToken(user);
+
+        res.cookie("token",token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite:"strict",
+            path:"/",
+            maxAge: 24*60*60*1000
+        });
+
         res.json({success:true,
             message:'User Logged In Successfully', 
-             token:generateToken(user),
             data:user,
         });
     } catch (error) {
