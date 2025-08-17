2db100d8b9b567eb20c03140d4782827ac5612e2
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-08-17 22:21:39 +0545

    minor change

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,7 +38,7 @@ export const loginUser = async (req,res) => {
 
         res.cookie("token",token,{
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: false,
             sameSite:"strict",
             path:"/",
             maxAge: 24*60*60*1000
