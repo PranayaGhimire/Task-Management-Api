c884fc378813c3b51feb7e15b520373c46d32243
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-08-28 20:17:50 +0545

    Minor changes

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/", async (req,res) => res.send("<h2>API Working Fine !!</h2><h3>Endpoints:</h3><h4>/api/auth</h4<br><h4>/api/tasks</h4>"))
+router.get("/", async (req,res) => res.send(`
+    <h2>API Working Fine !!</h2>
+    <h3>Endpoints:</h3>
+    <h4>/api/auth</h4<br>
+    <h4>/api/tasks</h4>
+    `))
 export default router;
