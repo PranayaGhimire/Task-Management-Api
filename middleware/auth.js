2c877d24f3cb33f34980fcf1de00a3285c9b3d04
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-08-28 21:41:52 +0545

    New change

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const protect = async (req,res,next) => {
-    // const authHeader = req.headers.authorization;
-    // if(!authHeader || !authHeader.startsWith('Bearer'))
-    //     return res.status(401).json( { message: 'Not Authorized'} );
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer'))
+        return res.status(401).json( { message: 'Not Authorized'} );
 
     try {
-        const token = req.cookies.token;
-        if (!token) return res.status(401).json({message:'Not Authorized'});
+        // const token = req.cookies.token;
+        // if (!token) return res.status(401).json({message:'Not Authorized'});
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id).select('-password');
         next();
